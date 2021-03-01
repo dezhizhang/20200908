b96d7fc40e1f29cc5d90031f5305bf0ac89ef811
dezhizhang <1541609448@qq.com>
2021-03-01 22:52:10 +0800

    提交文件

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-01 22:46:08
+ * @LastEditTime: 2021-03-01 22:51:17
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /20200908/app/controller/default/home.js
+ */
 'use strict';
 
 const Controller = require('egg').Controller;
@@ -5,7 +13,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    await this.ctx.render('/default/index')
+    await this.ctx.render('/index')
   }
 }
 
