fb1dec75b987a1968d85d81350e334fd15ba6436
dezhizhang <1541609448@qq.com>
2021-03-02 20:47:37 +0800

    添加图层

--- app/router.js
+++ app/router.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-01 22:46:08
+ * @LastEditTime: 2021-03-02 20:41:12
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /20200908/app/router.js
+ */
 'use strict';
 
 /**
@@ -6,4 +14,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.default.home.index);
+  router.get('/photograph', controller.default.photograph.index);
 };
